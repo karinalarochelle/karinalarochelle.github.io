2dadb7a291d01d1fb1ec2b27b261e495f68233bb
karinalarochelle <klarochelle@uri.edu>
2024-02-26 09:53:53 -0500

    updated profile app

--- script.js
+++ script.js
@@ -4,19 +4,19 @@ import { ref, onValue } from "https://www.gstatic.com/firebasejs/9.6.3/firebase-
 // script.js
 // Replace these with your actual Firebase configuration
 const firebaseConfig = {
-    apiKey: "AIzaSyC4F6sQ9kraEhyjb-gXI9ZS5CCdonaoCO8",
-    authDomain: "profile-app-e1282.firebaseapp.com",
-    databaseURL: "https://profile-app-e1282-default-rtdb.firebaseio.com",
-    projectId: "profile-app-e1282",
-    storageBucket: "profile-app-e1282.appspot.com",
-    messagingSenderId: "4762613742",
-    appId: "1:4762613742:web:901ee20544d718fc4000ff"
+    apiKey: "AIzaSyBNFGbJvf3Q77sLb_adDXBADRdGvtV78Og",
+    authDomain: "profile-app-14af9.firebaseapp.com",
+    databaseURL: "https://profile-app-14af9-default-rtdb.firebaseio.com",
+    projectId: "profile-app-14af9",
+    storageBucket: "profile-app-14af9.appspot.com",
+    messagingSenderId: "527016078505",
+    appId: "1:527016078505:web:7b551e3e1418d5ce5f4db8"
   };
 
 const app = initializeApp(firebaseConfig);
 const database = getDatabase(app);
 
-const userIds = ["CJJ7GOzLxRZhX3lfYvyTqs47PGz1", "Zo6eYFlIXxOo1YC4SaaTHWh5Aux2"]; // Replace with your two user IDs
+const userIds = ["BuZaEsFQqXUhvI3zh6dzAnNBq562", "beDf5JtbkEMWpkiA3vD52FayBSt1"]; // Replace with your two user IDs
 
 function displayUserProfile(userId) {
     const userRef = ref(database, `/${userId}`);
